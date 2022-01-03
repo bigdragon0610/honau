9dcf378e3e2fdbdcb270d66bd910dc85634dea0b
bigdragon0610 <74942852+bigdragon0610@users.noreply.github.com>
2022-01-04 02:52:00 +0900

    ファイルを追加

---
+++ honahonau.js
@@ -0,0 +1,8 @@
+const usamaru = document.getElementById('usamaru_img');
+const honau = document.getElementById('honau_title');
+const heart_wrapper = document.getElementById('heart_wrapper');
+const heart = "<p class='heart'>♡</p>";
+usamaru.addEventListener('click', () => {
+    honau.classList.toggle('color_red');
+    heart_wrapper.insertAdjacentHTML('beforeend', heart);
+});
---
+++ honaulogin.js
@@ -0,0 +1,22 @@
+let honau_name = [
+    'ほなう',
+    'ほなみ',
+    'ほなほな',
+    'ほなほなほなう',
+    'かわいいほなう',
+    'うちだほなみ',
+    '帆南',
+    '内田',
+    '内田帆南',
+    'hona',
+    'honau',
+    'honami',
+    'honahonau',
+]
+const login_button = document.getElementById('login_button');
+login_button.addEventListener('click', () => {
+    let login_name = document.getElementById('login_form').value;
+    if (honau_name.includes(login_name)) {
+        document.location.href = '../honahonau/honahonau.html';
+    }
+});
