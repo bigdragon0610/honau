f51d746df54ebcbbba5f5f61bd1959e8a2ca7ae7
bigdragon <ryudai-anada@keio.jp>
2022-01-08 19:53:35 +0900

    phpに変更

--- honaulogin/honaulogin.js
+++ honaulogin/honaulogin.js
@@ -22,6 +22,6 @@ const login_button = document.getElementById('login_button');
 login_button.addEventListener('click', () => {
   let login_name = document.getElementById('login_form').value;
   if (honau_name.includes(login_name)) {
-    document.location.href = '../honahonau/honahonau.html';
+    document.location.href = '../honahonau/honahonau.php';
   }
 });
